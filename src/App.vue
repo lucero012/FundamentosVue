585276a8d8c731ccb305575aee5256f2860952c2
Lucero <90294297+lucero012@users.noreply.github.com>
2025-03-19 16:07:39 -0600

    Subida inicial del proyecto FundamentosVue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+
+  <HelloWorld msg="Vite + Vue" />
+  <br> <br>
+  <img src="/lu.jpg" alt="Vue logo" />
+
+</template>
+
+<style scoped></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<script setup>
+// metodos 
+</script>
+
+<template>
+  <!-- estructura html -->
+   <h1>Hola Mundo</h1>
+  <button class="btn">Hola soy un botón</button> 
+ </template>
+
+<style scoped>
+/* estilos */
+h1
+{
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  font-size: 45px;
+  color: rgb(0, 255, 251);
+}
+.image {
+  display: block;
+  margin: 20px auto;
+  width: 150px;
+  height: 150px;
+}
+.btn
+{
+  background-color: blue;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+} 
+</style>
+
